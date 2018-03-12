ede8a29500c798ba118557e1ff048ccf93ef2d96
Theo Seguier <tseguier@student.42.fr>
2018-03-12 03:09:06 +0100

    [ FEATURE | TESTS ] Overridables data processing functions (#1)

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import { normalize } from 'normalizr';
 
 export default (options) => {
-  const opts = options || { actionFilter: /_FULFILLED$/ };
+  const opts = {
+    actionFilter: null,
+    onNormalizeData: (store, action, normalizedData) => normalizedData,
+    onNextAction: (store, action, normalizedData) => ({
+      ...action,
+      payload: {
+        ...action.payload,
+        data: normalizedData,
+      },
+    }),
+    ...options,
+  };
+
   return (store => next => (action) => {
     if (
       action.payload
-    && action.payload.data
-    && action.meta
-    && action.meta.schema
-    && (!opts.actionFilter
-      || (action.type
-        && action.type.match(opts.actionFilter)))) {
-      const normalizedData = normalize(action.payload.data, action.meta.schema);
-      store.dispatch({
-        type: 'STORE_UPDATE',
-        payload: normalizedData.entities,
-      });
-      return next({
-        ...action,
-        payload: {
-          ...action.payload,
-          data: normalizedData.result,
-        },
-      });
+      && action.payload.data
+      && action.meta
+      && action.meta.schema
+      && (!opts.actionFilter
+        || (action.type
+          && action.type.match(opts.actionFilter)))) {
+      let normalizedData = normalize(action.payload.data, action.meta.schema);
+      normalizedData = opts.onNormalizeData(store, action, normalizedData);
+      return next(opts.onNextAction(store, action, normalizedData));
     }
     return next(action);
   });
